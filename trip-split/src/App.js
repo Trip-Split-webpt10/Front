9c8ed3553fa7fa6fc2044efc6c2f2734223fbd22
lesley <lesleybanadzem2017@gmail.com>
2019-09-20 16:00:56 -0700

    hitup the pixabay api for images and xreated a carousel to loop through 20 images after every 3 seconds

--- trip-split/src/App.js
+++ trip-split/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from './Components/Header/Header'
 import './App.css';
+import Jumbotron from './Components/HomePage/Jumbotron/Jumbotron';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Jumbotron />
     </div>
   );
 }
---
+++ trip-split/src/Components/HomePage/Jumbotron/Carousel.js
@@ -0,0 +1,36 @@
+import React, {useEffect, useState} from 'react'
+
+function Carousel(props) {
+    const [index, setIndex] = useState(0)
+    useEffect(()=>{
+        const interval = setInterval(()=>{
+            if(index  >= 19){ 
+                setIndex(0)
+            } else{
+                setIndex(index + 1);
+            }
+        }, 3000);
+        return () => clearInterval(interval)
+      }, [index]);
+    
+    console.log(index)
+    const imgStyles = {
+        height: '700px',
+        width: '90%',
+        margin: '0 auto'
+    }
+    
+    function image(){
+        if (!props.randomImages[index]){ return ''}
+        else{
+           return  props.randomImages[index].largeImageURL;
+        }
+    }
+    return (
+        <>
+            <img src ={`${image()}`} style={imgStyles} alt =''/>
+        </>
+    )
+}
+
+export default Carousel
