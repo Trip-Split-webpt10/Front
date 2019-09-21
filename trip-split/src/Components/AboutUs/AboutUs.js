e9c56f4c15cc39aa4b5f2068e202828f47be6a87
Dtown <Dtown@DANTOWN>
2019-09-21 12:28:42 -0500

    CreatorCardPage

---
+++ trip-split/src/Components/AboutUs/AboutUs.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import styled from 'styled-components';
+import Header from './Header';
+import CardBox from './CardBox';
+
+const AboutUsCard = styled.div`
+width: 100%;
+height: 1500px;
+background-color: #FAFAFA;
+`
+
+function AboutUs() {
+  return (
+    <AboutUsCard>
+        <Header/>
+        <CardBox/>
+    </AboutUsCard>
+  );
+}
+
+export default AboutUs;
---
+++ trip-split/src/Components/AboutUs/persondata.js
@@ -0,0 +1,42 @@
+const persondata = [
+    {
+        "name": "Danny Town",
+        "position": "Front End Developer",
+        "picture": "https://inbound.citius.in/assets/front-end/img/personal_default_avatar.png",
+        "fact": "Insert Fact"
+    },
+    {
+        "name": "Lesley Banadzem",
+        "position": "Front End Developer",
+        "picture": "https://inbound.citius.in/assets/front-end/img/personal_default_avatar.png",
+        "fact": "Insert Fact"
+    },
+    {
+        "name": "Ana Araujo",
+        "position": "Front End Developer",
+        "picture": "https://inbound.citius.in/assets/front-end/img/personal_default_avatar.png",
+        "fact": "Insert Fact"
+    },
+    {
+        "name": "Tony Odele",
+        "position": "Front End Developer",
+        "picture": "https://inbound.citius.in/assets/front-end/img/personal_default_avatar.png",
+        "fact": "Insert Fact"
+    },
+    {
+        "name": "Nolu",
+        "position": "Front End Developer",
+        "picture": "https://inbound.citius.in/assets/front-end/img/personal_default_avatar.png",
+        "fact": "Insert Fact"
+    },
+    {
+        "name": "Christopher Wernli",
+        "position": "Back End Developer",
+        "picture": "https://inbound.citius.in/assets/front-end/img/personal_default_avatar.png",
+        "fact": "Insert Fact"
+    }
+
+]
+
+
+export default persondata;
