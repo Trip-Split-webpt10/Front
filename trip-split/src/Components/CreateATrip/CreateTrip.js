0c8f0d8fd8dff81f88eb07de6a28e31b2aba8765
Danny Town <47186976+dannyotown@users.noreply.github.com>
2019-09-23 19:55:45 -0400

    Form Start
    beginning of form creation

--- trip-split/src/Components/CreateATrip/CreateTrip.js
+++ trip-split/src/Components/CreateATrip/CreateTrip.js
@@ -1,16 +1,24 @@
 import React from 'react';
 import './CreateTrip.css';
+import CreateATripForm from './CreateTripForm';
 
 
 function CreateATrip() {
     function hideBackground(){
-        let backgroundgif = document.getElementById('backgroundhide')
+        let backgroundgif = document.getElementById('BackgroundHide')
         backgroundgif.style.display = 'none';
+        let tripform = document.getElementById('TripForm');
+        tripform.style.display = 'block';
     };
     return (
-        <div id="backgroundhide" className="backgroundgif">
+        <>
+        <div id="BackgroundHide" className="backgroundgif">
         <button onClick={hideBackground}>Split A Trip</button>
         </div>
+        <div id="TripForm">
+        <CreateATripForm/>
+        </div>
+        </>
     );
 }
 
--- trip-split/src/Components/CreateATrip/CreateTripForm.js
+++ trip-split/src/Components/CreateATrip/CreateTripForm.js
@@ -1,17 +1,53 @@
-import React from 'react';
-import './CreateTrip.css';
+import React, {useState, useEffect} from 'react'
+import { withFormik, Form, Field } from 'formik';
+import * as yup from 'yup';
+import Axios from 'axios';
+import styled from 'styled-components';
 
+const FormDisplayFlex = styled.div`
+    width: 50%;
+    display: flex;
+    flex-flow: column nowrap;
+    margin: 0 auto;
+    margin-top: 5%;
+`;
 
-function CreateATrip() {
-    function hideBackground(){
-        let backgroundgif = document.getElementById('backgroundhide')
-        backgroundgif.style.display = 'none';
-    };
-    return (
-        <div id="backgroundhide" className="backgroundgif">
-        <button onClick={hideBackground}>Split A Trip</button>
-        </div>
-    );
+const CreateATripForm = ({errors, touched}) => {
+  return (
+    <Form>
+    <FormDisplayFlex>
+      {touched.NameOfTrip && errors.NameOfTrip && <p className="error">{errors.NameOfTrip}</p>}
+      <Field type="text" name="NameOfTrip" placeholder="Name Of Trip" />
+      {touched.FriendAmount && errors.FriendAmount && <p className="error">{errors.FriendAmount}</p>}
+      <Field type="number" name="FriendAmount" placeholder="Amount Of Friends" />
+      {touched.AmountOfExpenses && errors.AmountOfExpenses && <p className="error">{errors.AmountOfExpenses}</p>}
+      <Field type="number" name="AmountOfExpenses" placeholder="How Many Expenses?" />
+      <button type="submit">Submit</button>
+      </FormDisplayFlex>
+    </Form>
+  )
 }
 
-export default CreateATrip;
+export default withFormik({
+  mapPropsToValues: (values) => {
+    return {
+      NameOfTrip: values.NameOfTrip || '',
+      FriendAmount: values.FriendAmount || '',
+      AmountOfExpenses: values.diet || ''
+    }
+  },
+  validationSchema: yup.object().shape({
+    NameOfTrip: yup.string().required('Species is a required field!'),
+    FriendAmount: yup.number().positive().required('FriendAmount is a required field!'),
+    AmountOfExpenses: yup.number().positive().required('FriendAmount is a required field!')
+  }),
+  handleSubmit:(values, {setStatus})=>{
+    Axios.post('', values)
+    .then((res)=>{
+      setStatus(res.data)
+    })
+    .catch((error)=>{
+      console.log(error)
+    })
+  }
+})(CreateATripForm)
