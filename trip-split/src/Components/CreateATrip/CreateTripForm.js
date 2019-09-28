b228522f6b3fa2d73df613b66074113b329f0f55
Dtown <dannyotown@gmail.com>
2019-09-28 12:21:09 -0500

    Update CreateTripForm.js

--- trip-split/src/Components/CreateATrip/CreateTripForm.js
+++ trip-split/src/Components/CreateATrip/CreateTripForm.js
@@ -24,21 +24,36 @@ const Header = styled.h1`
     font-family: 'Quicksand', sans-serif;
 `;
 
+const Border = styled.div`
+    border: 1px solid #D3D3D3;
+    padding: 5% 5% 15% 5%;
+    border-radius: 5px;
+    box-shadow: 8px 8px 3px lightgrey;
+`;
+
+const Card = styled.div`
+    padding: 6%;
+`;
+
 const CreateATripForm = ({ errors, touched }) => {
     return (
         <>
-            <Header>Create A Trip</Header>
-            <Form>
-                <FormDisplayFlex>
-                    {touched.destination && errors.destination && <p className="error">{errors.destination}</p>}
-                    <Field type="text" id='fieldstyle' name="destination" placeholder="destination" />
-                    {touched.start_date && errors.start_date && <p className="error">{errors.start_date}</p>}
-                    <Field type="date" id='fieldstyle' name="start_date" placeholder="Start Date" />
-                    {touched.end_date && errors.end_date && <p className="error">{errors.end_date}</p>}
-                    <Field type="date" id='fieldstyle' name="end_date" placeholder="End Date" />
-                    <button type="submit">Submit</button>
-                </FormDisplayFlex>
-            </Form>
+        <Card>
+            <Border>
+                <Header>Create A Trip</Header>
+                <Form>
+                    <FormDisplayFlex>
+                        {touched.destination && errors.destination && <p className="error">{errors.destination}</p>}
+                        <Field type="text" id='fieldstyle' name="destination" placeholder="destination" />
+                        {touched.start_date && errors.start_date && <p className="error">{errors.start_date}</p>}
+                        <Field type="date" id='fieldstyle' name="start_date" placeholder="Start Date" />
+                        {touched.end_date && errors.end_date && <p className="error">{errors.end_date}</p>}
+                        <Field type="date" id='fieldstyle' name="end_date" placeholder="End Date" />
+                        <button type="submit">Submit</button>
+                    </FormDisplayFlex>
+                </Form>
+            </Border>
+            </Card>
         </>
     )
 }
@@ -56,14 +71,14 @@ export default withFormik({
         start_date: yup.string().required('Start Date is a required field'),
         end_date: yup.string().required('End Date is a required field!')
     }),
-    handleSubmit: (values, {resetForm}) => {
+    handleSubmit: (values, { resetForm }) => {
         Axios.post('https://trip-split-api.herokuapp.com/api/trips', values)
             .then(() => {
                 resetForm();
-                return MySwal.fire({type:'success', title:'Trip Created Successfully',text:'Good Job!'})
+                return MySwal.fire({ type: 'success', title: 'Trip Created Successfully', text: 'Good Job!' })
             })
             .catch((error) => {
-                return MySwal.fire({type:'error', title:'There was an error',text:'Try Again!'})
+                return MySwal.fire({ type: 'error', title: 'There was an error', text: 'Try Again!' })
             })
     }
 })(CreateATripForm)
